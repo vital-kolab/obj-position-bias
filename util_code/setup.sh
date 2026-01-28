22d788e80cc94a98ace85f451de69018600ec9dc
LizaYaku <134000379+LizaYaku@users.noreply.github.com>
2026-01-28 12:31:43 -0500

    Update setup.sh

--- util_code/setup.sh
+++ util_code/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-module load python/3.11.5 # intended to work with compute canada, adjust for your system
+module load python/3.11.5 # intended to work with a Digital Research Alliance of Canada system and should be adjust for the capabilities of your system
 
 ENV_PATH="/home/eyakub/scratch" # adjust for your system
 WORKING_PATH="/home/eyakub/projects/def-kohitij/eyakub/obj-position-bias" # adjust for your system
@@ -13,7 +13,8 @@ source $ENV_PATH/.obj_pos_venv/bin/activate
 
 echo "Installing requirements"
 
-pip install --no-index -r $WORKING_PATH/util_code/requirements.txt
+# NOTE: this code was written for and run on a Digital Research Alliance of Canada system, if your system is not part of this network, remove '+computecanada' from requirements.txt
+pip install --no-index -r $WORKING_PATH/util_code/requirements.txt 
 
 echo "Env has been set up"
 
